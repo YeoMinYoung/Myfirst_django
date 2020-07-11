2a3871fc854a507432717d566a6652fe9d217027
YeoMinYoung <dufmasky@gmail.com>
2020-07-11 18:08:59 +0900

    add model

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200) 
+    #CharField max_length = 200  = 짧은 글
+    created_at = models.DateTimeField(auto_now_add=True)
+    #DateTimeField = 지금시간 자동으로 추가 허용
+    updated_at = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Blog
 # Create your views here.
 # hello/views.py
 
 def home(request): #view는 기능이기 때문에 함수로 구현
   #기본 홈페이지 만 띄우기여서 return만 해줌
-    return render(request, 'home.html')
+    blogs = Blog.objects 
+    #view에서만 사용할 변수를 만들어 model의 객체를 넣어주기,
+    #html에서 변수 return
+    return render(request, 'home.html',{'blogs':blogs})
+
+def new(request):
+  return render(request, 'new.html')
+
+def create(request):
+  if(request.method == 'POST'):
+    post = Blog()
+    post.title = request.POST['title']
+    post.body = request.POST['body']
+    post.save()
+  return redirect('home')
